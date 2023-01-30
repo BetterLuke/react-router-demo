36c3f0b0a96428f80944849fef6469a5a78c0cdd
真是刘皇叔 <luke.bei.2015@gmial.com>
2023-01-30 21:28:30 +0800

    :sparkles: setup greeting page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import GreetingPage from "./pages/GreetingPage";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/greetings",
+    element: <GreetingPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/GreetingPage.jsx
@@ -0,0 +1,3 @@
+export default function GreetingPage() {
+  return <p>GreetingPage</p>;
+}
